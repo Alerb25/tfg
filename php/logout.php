d31767e79f67a0b8de0747b538bdbdd1193f9246
Alerb25 <aruizbermudez05@gmail.com>
2025-04-17 17:21:33 +0200

    modified:   php/logout.php 	modified:   php/panel.php

---
+++ php/logout.php
@@ -0,0 +1,15 @@
+
+<?php
+// Iniciar sesión
+session_start();
+
+//Eliminar variables de sesión
+session_unset();
+
+//Destruir sesión
+session_destroy();
+
+header("Location: login.php");
+exit();
+
+?>
