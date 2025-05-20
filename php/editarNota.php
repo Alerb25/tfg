7ce3d6c2b1f75635d6acf21449d24770897351e5
Ale <aruizbermudez05@gamil.com>
2025-05-20 11:38:39 +0200

    modified:   php/editarNota.php

--- php/editarNota.php
+++ php/editarNota.php
@@ -18,41 +18,38 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["id_note"])) {
 
     // Verifica que la nota pertenezca al usuario
     $consulta = pg_query($conexion, "
-     SELECT n.* FROM nota n
-     LEFT JOIN compartir c ON n.id_notes = c.id_notes
-     WHERE n.id_notes = $idNota AND (
-         n.id_user = $id_user OR (c.id_user = $id_user AND c.permisos = 'edicion')
-     )
- ");
-
- $consulta = pg_query($conexion, $sql);
-
- if (!$consulta) {
-     die("Error en la consulta: " . pg_last_error($conexion));
- }
- 
- if (pg_num_rows($consulta) === 1) {
-     // todo bien
-     $nota = pg_fetch_assoc($consulta);
- }
-
- 
-
-// Si el usuario envía el formulario con el nuevo contenido
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["editar_nota"])) {
-    $idNota = intval($_POST["id_note"]);
-    $nuevoContenido = pg_escape_string($conexion, $_POST["contenido"]);
-    $fecha = date("Y-m-d");
-
-    $update = pg_query($conexion, "
-    UPDATE nota SET contenido = '$nuevoContenido', fecha_editado = '$fecha'
-    WHERE id_notes = $idNota AND (
-        id_user = $id_user OR EXISTS (
-            SELECT 1 FROM compartir 
-            WHERE id_notes = nota.id_notes AND id_user = $id_user AND permisos = 'edicion'
+        SELECT n.* FROM nota n
+        LEFT JOIN compartir c ON n.id_notes = c.id_notes
+        WHERE n.id_notes = $idNota AND (
+            n.id_user = $id_user OR (c.id_user = $id_user AND c.permisos = 'edicion')
         )
-    )
-");
+    ");
+
+    if (!$consulta) {
+        die("Error en la consulta: " . pg_last_error($conexion));
+    }
+
+    if (pg_num_rows($consulta) === 1) {
+        $nota = pg_fetch_assoc($consulta);
+    } else {
+        die("No tienes permiso para editar esta nota.");
+    }
+
+    // Si el usuario envía el formulario con el nuevo contenido
+    if (isset($_POST["editar_nota"])) {
+        $nuevoContenido = pg_escape_string($conexion, $_POST["contenido"]);
+        $fecha = date("Y-m-d");
+
+        $update = pg_query($conexion, "
+            UPDATE nota SET contenido = '$nuevoContenido', fecha_editado = '$fecha'
+            WHERE id_notes = $idNota AND (
+                id_user = $id_user OR EXISTS (
+                    SELECT 1 FROM compartir 
+                    WHERE id_notes = nota.id_notes AND id_user = $id_user AND permisos = 'edicion'
+                )
+            )
+        ");
+    }
 }
 ?>
 
